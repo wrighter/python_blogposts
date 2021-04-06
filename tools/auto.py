e3368cd2483f22ccc063b36f18fc028266211983
Matt Wright <matt@wrighters.net>
2021-04-05 22:29:08 -0500

    adding %autoreload article

---
+++ tools/auto.py
@@ -0,0 +1,4 @@
+
+def my_api(model, year):
+    # dummy result
+    return { 'model': model, 'year': year,}
---
+++ tools/auto2.py
@@ -0,0 +1,4 @@
+
+def my_api2(model, year):
+    # dummy result
+    return { 'model': model, 'year': year, }
