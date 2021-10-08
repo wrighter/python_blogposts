f95744be43ac11a9de7832570279728eeb19742d
wrighter <matt@wrighters.net>
2021-10-08 13:42:52 -0500

    add article on jupytext

---
+++ tools/jupyter_git_example.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import matplotlib.pyplot as plt
+plt.plot([x**0.5 for x in range(100)])
+
+# %%
